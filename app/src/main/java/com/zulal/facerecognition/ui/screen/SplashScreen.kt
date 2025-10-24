be76bcd010955a56c60b4ce94f4633d0673423d8
zulal.macit <zulal.macit@fluttersea.com>
2025-10-24 17:06:12 +0300

    Splash screen with ui design updated

--- app/src/main/java/com/zulal/facerecognition/ui/screen/SplashScreen.kt
+++ app/src/main/java/com/zulal/facerecognition/ui/screen/SplashScreen.kt
@@ -1,10 +1,17 @@
 package com.zulal.facerecognition.ui.screen
-
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.*
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
 import androidx.navigation.NavController
+import com.zulal.facerecognition.R
 import kotlinx.coroutines.delay
-
 @Composable
 fun SplashScreen(navController: NavController) {
     LaunchedEffect(Unit) {
@@ -12,6 +19,31 @@ fun SplashScreen(navController: NavController) {
         navController.navigate("camera"){
             popUpTo("splash"){ //Şu ekrana kadar olan geçmişi temizle, Splashı da sil
                 inclusive = true
+            }
+        }
+    }
+    Box(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color(0xFF1A1450)),
+        contentAlignment = Alignment.Center
+    ) {
+        Column(
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.Center
+        ) { // Orta kısımda yüz ikonu
+            Box(
+                modifier = Modifier
+                    .size(250.dp)
+                    .background(Color(0xFF231A60)),
+                contentAlignment = Alignment.Center
+            )
+            {
+                Image(
+                painter = painterResource(id = R.drawable.splash_face),
+                contentDescription = "Face Icon",
+                modifier = Modifier.size(240.dp)
+            )
             }
         }
     }
